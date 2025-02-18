3666c219fc6464d90e4f623961af45c03288e508
huangxin <1411423062@qq.com>
2025-02-18 23:11:38 +0800

    员工编辑

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -14,4 +16,12 @@ public interface EmployeeService {
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 //新增员工
     void save(EmployeeDTO employeeDTO);
+//分页查询
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void startOrstop(Integer status, Long id);
+    // 根据id查询
+    Employee getById(Long id);
+//bianjiyuangongxinxi
+    void update(EmployeeDTO employeeDTO);
 }
