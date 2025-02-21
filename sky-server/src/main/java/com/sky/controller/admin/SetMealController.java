7daa51c6546640c2484cc26ad35ca810af91127e
huangxin <1411423062@qq.com>
2025-02-22 00:31:37 +0800

    套餐开发

--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -37,6 +37,7 @@ public class SetMealController {
     @ApiOperation("删除套餐接口")
     public Result deleteMeal(@RequestParam List<Long> ids) {
         setMealService.deleteMealBatch(ids);
+
         return Result.success();
 
     }
