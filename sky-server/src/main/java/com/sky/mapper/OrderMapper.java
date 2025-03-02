934d7cbb5394246b27433678df5f21ab787e1086
huangxin <1411423062@qq.com>
2025-03-02 22:27:42 +0800

    下一个项目喽

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -53,4 +53,6 @@ public interface OrderMapper {
      */
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+    List<Orders> getByStatus(Integer pendingPayment, LocalDateTime time);
 }
